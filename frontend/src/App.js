447ec6236bd9f9f9de2647a7b44bfcb21827d28c
Zelyns <etienne.rou@outlook.fr>
2024-04-02 17:47:58 +0200

    pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import {BrowerRouter,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <BrowerRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory/>}/>
+        <Route path='/womens' element={<ShopCategory/>}/>
+        <Route path='/kids' element={<ShopCategory/>}/>
+      </Routes>
+      </BrowerRouter>
     </div>
   );
 }
