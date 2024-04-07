a3ad4719011a78150802482d261f827cc567f168
Zelyns <etienne.rou@outlook.fr>
2024-04-07 12:56:23 +0200

    32min

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import {BrowerRouter,Routes,Route} from 'react-router-dom';
+import { LoginSignup } from './Pages/LoginSignup';
+import { Product } from './Pages/Product';
+import { Cart } from './Pages/Cart';
+import { shop } from './Pages/Shop.jsx';
+import { ShopCategory } from './Pages/ShopCategory.jsx';
+import Navbar from './components/navbar/Navbar';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <BrowerRouter>
+      <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory/>}/>
-        <Route path='/womens' element={<ShopCategory/>}/>
-        <Route path='/kids' element={<ShopCategory/>}/>
+        <Route path='/' element={<shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="mens"/>}/>
+        <Route path='/womens' element={<ShopCategory category="womens"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='product' element={<Product/>} >
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
-      </BrowerRouter>
+      </BrowserRouter>
     </div>
   );
 }
