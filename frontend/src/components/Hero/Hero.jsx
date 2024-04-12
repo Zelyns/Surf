30a4dc6558035ac21d23d7efa5c9a0e9e675fb62
Enzo <enzolaborde91@gmail.com>
2024-04-12 13:26:35 +0200

    Prblm affichage acceuil a résoudre ou a laisser pour le moment vidéo : 1:00:00

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -8,17 +8,17 @@ const Hero = () => {
   return (
     <div className='hero'>
         <div className="hero-left">
-            <h2>Neew arrivals only</h2>
+            <h2>Nouveaux arrivages uniquement</h2>
             <div>
                 <div className="hero-hand-icon">
-                <p>new</p>
+                <p>nouvelle</p>
                 <img src={hand_icon} alt="" />
             </div>
             <p>collection</p>
-            <p>for everyone</p>
+            <p>pour tous</p>
             </div>
             <div className="hero-latest-btn">
-                <div>Latest Collection</div>
+                <div>Dernière collection</div>
                 <img src={arrow_icon} alt="" />
             </div>
         </div>
---
+++ frontend/src/components/assets/data.js
@@ -0,0 +1,37 @@
+import p1_img from './Surf-C-1.jpeg'
+import p2_img from './Surf-C-2.jpeg'
+import p3_img from './Surf-C-3.jpeg'
+import p4_img from './Surf-C-4.jpeg'
+
+let data_product = [
+    {
+        id:1,
+        name:"Elite - Combinaison de surf zip poitrine pour Femme",
+        image:p1_img,
+        new_price:300.00,
+        old_price:320.00
+    },
+    {
+        id:2,
+        name:"Swell Series 2022 - Combinaison de surf zip poitrine pour Femme",
+        image:p2_img,
+        new_price:250.00,
+        old_price:290.00
+    },
+    {
+        id:3,
+        name:"Swell Series 2022 - Combinaison de surf zip poitrine pour Femme",
+        image:p3_img,
+        new_price:250.00,
+        old_price:290.00
+    },
+    {
+        id:4,
+        name:"Swell Series 2022 - Combinaison back zip pour Femme",
+        image:p4_img,
+        new_price:230.00,
+        old_price:260.00
+    }
+];
+
+export default data_product;
