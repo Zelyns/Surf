7b197b919b00df12f627204c1a89f2c83341a739
Etienne Rousseau <etienne.rou@outlook.fr>
2024-04-15 23:13:12 +0200

    2.51

--- frontend/src/components/CartItems/CartItems.jsx
+++ frontend/src/components/CartItems/CartItems.jsx
@@ -21,18 +21,48 @@ const CartItems = () => {
       {all_product.map((e)=>{
         if(cartItems[e.id]>0){
           return <div>
-          <div className="cartitems-format">
+          <div className="cartitems-format cartitems-format-main">
             <img src={e.image} alt="" className='carticon-product-icon' />
             <p>{e.name}</p>
             <p>${e.new_price}</p>
             <button className='cartitems-quantity'>{cartItems[e.id]}</button>
-            <p>{e.new_price*[e.id]}</p>
-            <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
+            <p>${e.new_price*[e.id]}</p>
+            <img className='cartitmes-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
           </div>
           <hr /> 
         </div>
         }
+        return null;
       })}
+      <div className="cartitmes-down">
+        <div className="cartitmes-total">
+          <h1>Totals Panier</h1>
+          <div>
+            <div className="cartitmes-total-item">
+              <p>Total</p>
+              <p>€{0}</p>
+              <hr />
+              <div className="cartitmes-total-item">
+                <p>Frais d'expédition</p>
+                <p>Gratuit</p>
+              </div>
+              <hr />
+              <div className="cartitmes-total-item">
+                <h3>Total</h3>
+                <h3>${0}</h3>
+              </div>
+            </div>
+            <button>Passer en caisse</button>
+          </div>
+          <div className="cartitems-promocode">
+            <p>Code promo</p>
+            <div className="cartitems-promobox">
+              <input type="text" placeholder='Code' />
+              <button>Envoyer</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
